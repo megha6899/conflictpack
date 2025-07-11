d0ac9196abfbe71e20cfb75f8357e18f638a9b36
unknown <meghadevopsengineer@outlook.com>
2025-07-11 23:11:36 +0530

    feature coded

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.36.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {
+      
+    }
+    subscription_id = "4f66054f-49b5-4a50-97d4-b19124d09600"
+  # Configuration options
+}
