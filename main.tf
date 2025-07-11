b6b0f74d8a3c7245c2957a66fbb525c07419ab54
unknown <meghadevopsengineer@outlook.com>
2025-07-11 23:40:00 +0530

    third commit

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
 resource "azurerm_resource_group" "rg1" {
-  name     = "rg11"
-  location = "West Europe"
+  name     = "rg15"
+  location = "Japan East"
 }
 
 resource "azurerm_resource_group" "rg2" {
   name     = "rg12"
   location = "North Europe"
+}
+
+resource "azurerm_resource_group" "rg3" {
+  name     = "rg13"
+  location = "central us"
 }
