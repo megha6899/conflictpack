ab8331e28771db73cfef2d19a7bde0e5e70bb90a
unknown <meghadevopsengineer@outlook.com>
2025-07-12 00:37:30 +0530

    la de bro git conflict

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "rg34" {
+  name     = "rg3234"
+  location = "west europe"
+}
+
+resource "azurerm_resource_group" "rg35" {
+  name     = "rg323435"
+  location = "north europe"
+}
