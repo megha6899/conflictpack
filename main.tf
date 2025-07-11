34165837fc7ac70a6aaed0430b8355abadb5aa33
unknown <meghadevopsengineer@outlook.com>
2025-07-11 23:51:40 +0530

    comit 31

--- main.tf
+++ main.tf
@@ -1,14 +1,6 @@
-resource "azurerm_resource_group" "rg1" {
-  name     = "rg15"
-  location = "Japan East"
-}
 
-resource "azurerm_resource_group" "rg2" {
-  name     = "rg12"
-  location = "North Europe"
-}
 
-resource "azurerm_resource_group" "rg3" {
-  name     = "rg13"
+resource "azurerm_resource_group" "rg31" {
+  name     = "rg3131"
   location = "central us"
 }
